a8b554d27ac18c4d876495ff4de834aafb06fb7b
Fernando Maia <fermaiasoares@aol.com>
2022-06-26 01:37:45 -0300

    Configurando vite-copy para realizar o deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: 'src/assets/images', dest: 'assets' }
+      ]
+    })
+  ],
+  root: '.',
+  publicDir: 'assets',
+  build: {
+    emptyOutDir: true,
+    outDir: './dist',
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+
+        assetFileNames: ({name}) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')){
+              return 'assets/images/[name]-[hash][extname]';
+          }
+          
+          if (/\.css$/.test(name ?? '')) {
+              return 'assets/css/[name]-[hash][extname]';   
+          }
+
+          // default value
+          // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+          return 'assets/[name]-[hash][extname]';
+        },
+      }
+    }
+  }
 })
